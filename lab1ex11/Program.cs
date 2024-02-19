1cbf8a9af56e26d705a6d4b2309fa12d8f4e5491
victor.gozu <victor.gozu@MP150159>
2024-02-19 19:33:30 +0200

    Add project files.

---
+++ lab1ex11/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+* Folosind instructiunea "switch", scrieti un program care citind de la tastatura
+* un numar intreg va verifica urmatoarele
+* - "unu" daca numarul intreg este 1
+* - "doi" daca numarul intreg este 2
+* - "trei" daca numarul intreg este 3
+* - "cinci" daca numarul intreg este 5
+* - "opt" daca numarul intreg este 8
+* - "neidentificat" pentru orice alt caz
+*/
+
+
+Console.WriteLine("Introduceti un numar intreg:");
+int numar = int.Parse(Console.ReadLine());
+
+switch (numar)
+{
+    case 1:
+        Console.WriteLine("unu");
+        break;
+    case 2:
+        Console.WriteLine("doi");
+        break;
+    case 3:
+        Console.WriteLine("trei");
+        break;
+    case 5:
+        Console.WriteLine("cinci");
+        break;
+    case 8:
+        Console.WriteLine("opt");
+        break;
+    default:
+        Console.WriteLine("neidentificat");
+        break;
+}
